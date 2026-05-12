4e1e6bc01754b80060c61c839507f4b77ef67a47
Rifah Sajida Deya <rifahsajida7@gmail.com>
2026-05-12 18:52:15 +0600

    feat: model update

--- src/services/ai.service.ts
+++ src/services/ai.service.ts
@@ -7,7 +7,7 @@ const log = createLogger("AIService");
 const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
 //const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash",
   generationConfig: {
     responseMimeType: "application/json",
     temperature: 0.2,
